1f646972c3155a07b3d1a90ffad827498d7d7f61
Anderson Cespedes <andersontrabajos2@gmail.com>
2023-08-10 21:28:27 -0500

    review ya sirve como el profesro envio

--- Env.cs
+++ Env.cs
@@ -1,7 +1,10 @@
 namespace EjercicioLINQ1;
 using EjercicioLINQ1.Class;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.Json;
 
-    public static class Env
+public static class Env
     {
         private static string fileName = "productos.json";
         private static List<Productos> productos = new();
@@ -9,4 +12,76 @@ using EjercicioLINQ1.Class;
         public static string FileName { get => fileName; set => fileName = value; }
         public static List<Productos> Productos { get => productos; set => productos = value; }
         
+        public static void LoadDataProductos(string nombreArchivo, List<Productos> listProducts)
+        {
+            if (!File.Exists(nombreArchivo))
+            {
+                File.WriteAllText(nombreArchivo, "");
+                Productos = listProducts;
+                return;
+            }
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(nombreArchivo))
+                {
+                    string json = reader.ReadToEnd();
+                    if (!string.IsNullOrEmpty(json))
+                    {
+                        Productos = JsonSerializer.Deserialize<List<Productos>>(json, new JsonSerializerOptions
+                        {
+                            PropertyNameCaseInsensitive = true
+                        }) ?? new List<Productos>();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while loading data: " + ex.Message);
+            }
+        }
+
+
+/* 
+if (!File.Exists(nombreArchivo))
+            {
+                File.WriteAllText(nombreArchivo, "");
+                Productos = listProducts;
+                return;
+            }
+
+            using (StreamReader reader = new StreamReader(nombreArchivo))
+            {
+                string json = reader.ReadToEnd();
+                Productos = JsonSerializer.Deserialize<List<Productos>>(json, new JsonSerializerOptions() 
+                { PropertyNameCaseInsensitive = true }) ?? new List<Productos>();
+            }
+
+
+if (!File.Exists(nombreArchivo))
+    {
+        File.WriteAllText(nombreArchivo, "");
+        Productos = listProducts;
+        return;
+    }
+
+    try
+    {
+        using (StreamReader reader = new StreamReader(nombreArchivo))
+        {
+            string json = reader.ReadToEnd();
+            if (!string.IsNullOrEmpty(json))
+            {
+                Productos = JsonSerializer.Deserialize<List<Productos>>(json, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                }) ?? new List<Productos>();
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Error while loading data: " + ex.Message);
+    }
+ */
     }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 ﻿using EjercicioLINQ1.Class;
 using EjercicioLINQ1;
-using Newtonsoft.Json;
+using System.Text.Json;
+using System.IO;
+using System;
+using System.Collections.Generic;
 internal class Program{
     private static void  Main(string[] args)
     {
+        //Env.LoadDataProductos(Env.FileName,);
         Productos p = new ();
         Guid idG = Guid.NewGuid();
         p.Id = idG.ToString();
@@ -14,8 +18,17 @@ internal class Program{
         p.StockMin = 10;
         p.StockMax = 20;
         Env.Productos.Add (p);
+        List<Productos> prod = new()
+        {
+            p
+        };
+        Env.LoadDataProductos(Env.FileName,prod);
 
-        string json = JsonConvert.SerializeObject(Env.Productos, Formatting.Indented);
+        string json = JsonSerializer.Serialize(Env.Productos, new JsonSerializerOptions
+        {
+            WriteIndented = true
+        });
+
         File.WriteAllText(Env.FileName, json);
     }
 }
